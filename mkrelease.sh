68b5d1f8a935828c05bed1f0f6e732e8a53a33ca
Fredrik Wikstrom <fredrik@a500.org>
2023-01-24 11:51:48 +0200

    Add zlib license to archive as well.

--- mkrelease.sh
+++ mkrelease.sh
@@ -11,6 +11,7 @@ else
   make -f makefile.aros all
 fi;
 
+ZLIBDIR='src/zlib-1.2.13'
 DESTDIR='tmp'
 
 rm -rf ${DESTDIR}
@@ -23,6 +24,7 @@ else
 fi;
 cp -p README ${DESTDIR}/z_lib
 cp -p LICENSE ${DESTDIR}/z_lib
+cp -p ${ZLIBDIR}/LICENSE ${DESTDIR}/z_lib/LICENSE-zlib
 cp -p releasenotes ${DESTDIR}/z_lib
 cp -p z.library.000 ${DESTDIR}/z_lib/Libs
 cp -p z.library.020 ${DESTDIR}/z_lib/Libs
@@ -31,6 +33,7 @@ cp -p icons/def_drawer.info ${DESTDIR}/z_lib.info
 cp -p icons/def_install.info ${DESTDIR}/z_lib/Install.info
 cp -p icons/def_doc.info ${DESTDIR}/z_lib/README.info
 cp -p icons/def_doc.info ${DESTDIR}/z_lib/LICENSE.info
+cp -p icons/def_doc.info ${DESTDIR}/z_lib/LICENSE-zlib.info
 cp -p icons/def_doc.info ${DESTDIR}/z_lib/releasenotes.info
 
 rm -rf z_lib.${HOST}.lha
