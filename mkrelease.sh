b72d034585ffa6dea4c175f1867ca319a4c87575
Fredrik Wikstrom <fredrik@a500.org>
2023-01-26 19:37:34 +0200

    Add header files to release archives.

--- mkrelease.sh
+++ mkrelease.sh
@@ -16,6 +16,14 @@ DESTDIR='tmp'
 
 rm -rf ${DESTDIR}
 mkdir -p ${DESTDIR}/z_lib/Libs
+mkdir -p ${DESTDIR}/z_lib/Developer/include/sfd
+mkdir -p ${DESTDIR}/z_lib/Developer/include/fd
+mkdir -p ${DESTDIR}/z_lib/Developer/include/include_h/clib
+mkdir -p ${DESTDIR}/z_lib/Developer/include/include_h/defines
+mkdir -p ${DESTDIR}/z_lib/Developer/include/include_h/inline
+mkdir -p ${DESTDIR}/z_lib/Developer/include/include_h/libraries
+mkdir -p ${DESTDIR}/z_lib/Developer/include/include_h/pragmas
+mkdir -p ${DESTDIR}/z_lib/Developer/include/include_h/proto
 
 if [ "$HOST" = "m68k-amigaos" ]; then
   cp -p Install ${DESTDIR}/z_lib
@@ -28,6 +36,14 @@ cp -p ${ZLIBDIR}/LICENSE ${DESTDIR}/z_lib/LICENSE-zlib
 cp -p releasenotes ${DESTDIR}/z_lib
 cp -p z.library.000 ${DESTDIR}/z_lib/Libs
 cp -p z.library.020 ${DESTDIR}/z_lib/Libs
+cp -p z_lib.sfd ${DESTDIR}/z_lib/Developer/include/sfd
+cp -p include/fd/z_lib.fd ${DESTDIR}/z_lib/Developer/include/fd
+cp -p include/clib/z_protos.h ${DESTDIR}/z_lib/Developer/include/include_h/clib
+cp -p include/defines/z.h ${DESTDIR}/z_lib/Developer/include/include_h/defines
+cp -p include/inline/z.h ${DESTDIR}/z_lib/Developer/include/include_h/inline
+cp -p include/libraries/z.h ${DESTDIR}/z_lib/Developer/include/include_h/libraries
+cp -p include/pragmas/z_pragmas.h ${DESTDIR}/z_lib/Developer/include/include_h/pragmas
+cp -p include/proto/z.h ${DESTDIR}/z_lib/Developer/include/include_h/proto
 
 cp -p icons/def_drawer.info ${DESTDIR}/z_lib.info
 cp -p icons/def_install.info ${DESTDIR}/z_lib/Install.info
